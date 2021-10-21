a220c0951eafdd8d18e999647cb53c8f0cfd67e3
hanshuliang <octopus_truth@163.com>
2021-10-21 20:41:56 +0800

    update

--- myapp-hotfix/src/main/java/com/example/myapp/Business.java
+++ myapp-hotfix/src/main/java/com/example/myapp/Business.java
@@ -21,12 +21,12 @@ public class Business {
      */
     public static void startEngine(Context base) {
         Log.i("HSL", "startEngine 方法入口");
-        /*try {
+        try {
             VirtualCore.get().startup(base);
             Log.i("HSL", "startEngine 启动 VA 引擎");
         } catch (Throwable e) {
             e.printStackTrace();
-        }*/
+        }
         Log.i("HSL", "startEngine 方法结束");
     }
 
@@ -38,12 +38,12 @@ public class Business {
         // int COMPARE_VERSION = 0X01 << 3;
         // int SKIP_DEX_OPT = 0x01 << 6;
         // 或运算结果 72
-        /*int flags = InstallStrategy.COMPARE_VERSION | InstallStrategy.SKIP_DEX_OPT;
+        int flags = InstallStrategy.COMPARE_VERSION | InstallStrategy.SKIP_DEX_OPT;
 
         // 安装 SD 卡根目录中的 app.apk 文件
         // /storage/emulated/0/app.apk
         VirtualCore.get().installPackage(context.getFilesDir() + "/app.apk", flags);
-        Log.i("HSL", "anzhuang 安装插件");*/
+        Log.i("HSL", "anzhuang 安装插件");
 
         Log.i("HSL", "anzhuang 安装插件 方法结束");
     }
@@ -55,11 +55,11 @@ public class Business {
         Log.i("HSL", "qidong 启动插件 方法入口");
 
         // 打开应用
-        /*Intent intent =  VirtualCore.get().getLaunchIntent("kim.hsl.svg", 0);
+        Intent intent =  VirtualCore.get().getLaunchIntent("kim.hsl.svg", 0);
         VActivityManager.get().startActivity(intent, 0);
         Log.i("HSL", "qidong 启动插件");
 
-        activity.finish();*/
+        activity.finish();
 
         Log.i("HSL", "qidong 启动插件 方法结束");
     }
